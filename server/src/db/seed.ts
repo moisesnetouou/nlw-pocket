fb29bc1d72ee5aeffbf2fee2c118eedac46bdb5e
Moisés <moisesnetored@gmail.com>
2024-09-10 23:00:30 -0400

    feat: seed database with initial data and clean up client connection

---
+++ server/src/db/seed.ts
@@ -0,0 +1,28 @@
+import { client, db } from '.'
+import { goals, goalsCompletions } from './schema'
+import dayjs from 'dayjs'
+
+async function seed() {
+  await db.delete(goalsCompletions)
+  await db.delete(goals)
+
+  const result = await db
+    .insert(goals)
+    .values([
+      { title: 'Acordar cedo', desiredWeeklyFrequency: 5 },
+      { title: 'Me exercitar', desiredWeeklyFrequency: 3 },
+      { title: 'Meditar', desiredWeeklyFrequency: 1 },
+    ])
+    .returning()
+
+  const startOfWeek = dayjs().startOf('week')
+
+  await db.insert(goalsCompletions).values([
+    { goaldId: result[0].id, createdAt: startOfWeek.toDate() },
+    { goaldId: result[1].id, createdAt: startOfWeek.add(1, 'day').toDate() },
+  ])
+}
+
+seed().finally(() => {
+  client.end()
+})
