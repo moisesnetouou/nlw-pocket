b1ef247bba25afadc1c6e3f1c6c8895b6abee33d
Moisés <moisesnetored@gmail.com>
2024-09-18 00:44:23 -0400

    feat: add route to get week summary

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -8,6 +8,7 @@ import {
 import { createGoalRoute } from './routes/create-goal'
 import { createCompletionRoute } from './routes/create-completion'
 import { getPendingGoalsRoute } from './routes/get-pending-goals'
+import { getWeekSummaryRoute } from './routes/get-week-summary'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -17,6 +18,7 @@ app.setSerializerCompiler(serializerCompiler)
 app.register(createGoalRoute)
 app.register(createCompletionRoute)
 app.register(getPendingGoalsRoute)
+app.register(getWeekSummaryRoute)
 
 app
   .listen({
